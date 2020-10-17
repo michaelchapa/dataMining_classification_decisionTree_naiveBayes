ff2d668a9f23c6b4ce4fe8961c0e7a83c1e54574
Michael Chapa <michaelchapa1@gmail.com>
2020-10-17 13:56:35 -0500

    Added reference documents, completed preprocessing utility function for NaiveBayes subRoutine. Extended previous utility subRoutines to have optional inputs.

--- hw6.py
+++ hw6.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from random import randrange
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 
 ####################### expandData_byCount ################################
 # Purpose: 
@@ -33,25 +37,75 @@ def expandData_byCount(df):
 #   If an attribute has three values 'A', 'B', 'C'; it encodes
 #   'A' as 0, 'B' as 1, 'C' as 2. 
 # Parameters:
-#   I       DataFrame       df
+#   I       DataFrame       data
+#   I       String || None  columnToDrop    attribute to remove from data
+#   I       List || None    toEncode               
 # Returns:
 #   DataFrame with Encoded attributes and non-categorical attributes.
 # Notes:
 #   The Encode section appends the encoded values,
 #   the Encoded column will be named '{attributeName}_code'.
-def encodeCategorical(df):
-    obj_df = df.select_dtypes(include = ['object']).copy()
-    categoricalAttr = list(obj_df.columns)
-    lb_make = LabelEncoder()
+def encodeCategorical(data, columnToDrop, toEncode):
+    lb_make = LabelEncoder() # Does the Encoding heavy-lifting
+    
+    if toEncode != None:
+        categoricalAttr = toEncode
+    else:
+        obj_df = data.select_dtypes(include = ['object']).copy()
+        categoricalAttr = list(obj_df.columns)
+    
+    if columnToDrop != None:
+        data = data.drop(columns = columnToDrop)
     
     # Encode
     for attr in categoricalAttr:
-        df[attr + '_code'] = lb_make.fit_transform(obj_df[attr])
+        data[attr + '_code'] = lb_make.fit_transform(data[attr])
         
-    # Remove categorical attributes
-    df = df.drop(columns = categoricalAttr)
+    # Remove non-encoded categorical attributes
+    data = data.drop(columns = categoricalAttr)
     
-    return df
+    return data
+
+
+#################### preProcess_naiveBayes_classifier ######################
+# Purpose:
+#   Replicates rows based on their count. (see subRoutine expandData_byCount)
+#   Encodes categorical data, takes discretized columns with ranges and
+#   Populates each value with a random value from that range (inclusive)
+#   Removes a specified column (school assignment request)
+# Parameters:
+#   I       DataFrame       data
+#   I       String || None  columnToDrop    attribute to remove from data
+# Returns:
+#   DataFrame
+# Notes:
+#   None == NULL
+def preProcess_naiveBayes_classifier(data, columnToDrop):
+    data = expandData_byCount(data)
+    
+    # deRange age and salary columns
+    ageList = []
+    salaryList = []
+    for instance in data.iterrows():
+        age = instance[1]['age'] # 1st dimension value is dataFrame index
+        salary = instance[1]['salary']
+        
+        age = tuple(map(int, age.split('..'))) # turns age into minMax tuple
+        ageList.append(randrange(age[0], age[1], 1)) # randomize
+        
+        salary = tuple(salary.split('..'))
+        salaryList.append(randrange(int(salary[0][:-1]), int(salary[1][:-1])))
+        
+    # replace all range values with numpy list
+    data['age'] = ageList 
+    data['salary'] = salaryList
+    
+    data = encodeCategorical(data, None, ['department'])
+    
+    if columnToDrop != None:
+        data = data.drop(columns = columnToDrop)
+        
+    return data
 
 
 ####################### create_displayDecisionTree #########################
@@ -61,7 +115,7 @@ def encodeCategorical(df):
 #   I       DataFrame       data
 #   I       String          target      Target Class attribute name
 # Returns:
-#   None
+#   DecisionTreeClassifier
 # Notes:
 #   The target class should be a column name of the dataFrame
 def create_displayDecisionTree(data, target):
@@ -71,19 +125,29 @@ def create_displayDecisionTree(data, target):
     plot_tree(clf, filled = True)
     plt.show()
     
+    return clf
+    
+
 ################ create_display_naiveBayes_classifier #####################
 # Purpose:
-#   ##
+#   Trains a Classifier using Gaussian Naive Bayes algorithm 
 # Parameters:
-#   ##
-#   ##
+#   I       DataFrame       data
+#   I       String          targetClass
 # Returns:
-#   ##
+#   GaussianNB
 # Notes:
 #   None
-def create_display_naiveBayes_classifier(data):
+def create_display_naiveBayes_classifier(data, targetClass):
+    gnb = GaussianNB()
+    
+    X = data.drop(columns = targetClass)
+    y = data[targetClass]
+    
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size = 0.1, random_state = 0)
+        
     return data
-
 
 
 def main():
@@ -101,13 +165,14 @@ def main():
     ['secretary', 'junior', '26..30', '26k..30k', 6]]
 
     df1 = pd.DataFrame(d[1:], columns = d[0])
-    df2 = expandData_byCount(df1)
-    df2 = encodeCategorical(df2)
     
-    # Per assignment, remove 'Count' column from df2
-    df2 = df2.drop(columns = 'count')
+    #df2 = expandData_byCount(df1)
+    #df2 = encodeCategorical(df2, 'count', None)
+        
+    #create_displayDecisionTree(df2, 'department_code')
     
-    create_displayDecisionTree(df2, 'department_code')
+    df3 = preProcess_naiveBayes_classifier(df1, 'count')
+    create_display_naiveBayes_classifier(df3, 'status')
     
 # Context the file is running in is __main__
 if __name__ == "__main__":
