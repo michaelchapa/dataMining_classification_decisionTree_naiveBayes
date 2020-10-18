680402961b52dc521721042640d109bc735ccb9b
Michael Chapa <michaelchapa1@gmail.com>
2020-10-18 08:10:40 -0500

    tweaked source file, added the binary tree classifier plot as a .png

--- hw6.py
+++ hw6.py
@@ -4,9 +4,7 @@ import matplotlib.pyplot as plt
 
 from random import randrange
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
 ####################### expandData_byCount ################################
@@ -67,6 +65,31 @@ def encodeCategorical(data, columnToDrop, toEncode):
     return data
 
 
+####################### create_displayDecisionTree #########################
+# Purpose:
+#   Creates a decision tree classifier, displays the tree, prints a prediction
+# Parameters:
+#   I       DataFrame       data
+#   I       String          target      Target Class attribute name
+# Returns:
+#   DecisionTreeClassifier
+# Notes:
+#   The target class should be a column name of the dataFrame
+def create_displayDecisionTree(data, target):
+    plt.figure(figsize=(15, 15)) 
+    clf = DecisionTreeClassifier(random_state = 0).fit( \
+                                 data.drop(columns = target), data[target])
+    plot_tree(clf, filled = True)
+    plt.show()
+    
+    if clf.predict([[3, 28, 50]]) == 0:
+        print('junior')
+    else:
+        print('senior')
+        
+    return clf
+
+
 #################### preProcess_naiveBayes_classifier ######################
 # Purpose:
 #   Replicates rows based on their count. (see subRoutine expandData_byCount)
@@ -106,31 +129,6 @@ def preProcess_naiveBayes_classifier(data, columnToDrop):
         data = data.drop(columns = columnToDrop)
         
     return data
-
-
-####################### create_displayDecisionTree #########################
-# Purpose:
-#   Creates a decision tree classifier, displays the tree, prints a prediction
-# Parameters:
-#   I       DataFrame       data
-#   I       String          target      Target Class attribute name
-# Returns:
-#   DecisionTreeClassifier
-# Notes:
-#   The target class should be a column name of the dataFrame
-def create_displayDecisionTree(data, target):
-    plt.figure(figsize=(15, 15)) 
-    clf = DecisionTreeClassifier(random_state = 0).fit( \
-                                 data.drop(columns = target), data[target])
-    plot_tree(clf, filled = True)
-    plt.show()
-    
-    if clf.predict([[3, 28, 50]]) == 0:
-        print('junior')
-    else:
-        print('senior')
-        
-    return clf
     
 
 ################ create_test_naiveBayes_classifier #####################
@@ -157,7 +155,7 @@ def create_test_naiveBayes_classifier(data, targetClass):
 
 
 def main():
-    d = [['department', 'status', 'age', 'salary', 'count'], 
+    data = [['department', 'status', 'age', 'salary', 'count'], 
     ['sales', 'senior', '31..35', '46k..50k', 30],
     ['sales', 'junior', '26..30', '26k..30k', 40],
     ['sales', 'junior', '31..35', '31k..35k', 40],
@@ -170,14 +168,10 @@ def main():
     ['secretary', 'senior', '46..50', '36k..40k', 4],
     ['secretary', 'junior', '26..30', '26k..30k', 6]]
 
-    df1 = pd.DataFrame(d[1:], columns = d[0])
+    df1 = pd.DataFrame(data[1:], columns = data[0])
     
     df2 = expandData_byCount(df1)
     df2 = encodeCategorical(df2, 'count', None)
-    
-    test = encodeCategorical(df1, 'count', None)
-    print(test)
-        
     create_displayDecisionTree(df2, 'department_code')
     
     df3 = preProcess_naiveBayes_classifier(df1, 'count')
